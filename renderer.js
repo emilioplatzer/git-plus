5e87c1646cf6af615d774fce3e0a32afaf9a70dc
Emilio Platzer <emilioplatzer@gmail.com>
2019-05-05 22:42:12 -0300

    Pone un ícono según el repositorio del package.json

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var fs = require('fs').promises;
+var html = require('js-to-html').html;
 
 /** @param {number} path */
 async function renderProject(path){
@@ -13,11 +14,39 @@ async function renderProject(path){
             var title = document.createElement('h2');
             div1.append(title);
             title.textContent = path; 
-            for(let file of files){
+            await Promise.all(files.map(async function(file){
                 var div2 = document.createElement('div');
                 div1.appendChild(div2);
-                div2.textContent=file;
-            }
+                try{
+                    let stat = await fs.stat(path+'/'+file);
+                    if(stat.isDirectory()){
+                        var img=html.img({class:'dir-type-icon', src:"img/loading-dir-type.png"}).create();
+                        div2.appendChild(img);
+                        div2.appendChild(html.span(file).create());
+                    }
+                    try{
+                        let packageJSON = await fs.readFile(path+'/'+file+'/package.json');
+                        let packageFile = JSON.parse(packageJSON);
+                        let repo = packageFile.repository && (packageFile.repository.url || packageFile.repository);
+                        if(/https?:\/\/gitlab/.test(repo)){
+                            img.src="https://about.gitlab.com/ico/favicon-32x32.png"
+                        }else if(/https?:\/\/github|^[^/]*\/[^/]*$/.test(repo)){
+                            img.src="https://github.com/fluidicon.png"
+                        }else{
+                            img.src="img/packagejson.png"
+                        }
+                    }catch(err){
+                        if(err.code=='ENOENT'){
+                            img.src="img/unknown-dir.png";
+                        }else{
+                            img.src="img/error.gif";
+                            img.title=err.message;
+                        }
+                    }
+                }catch(err){
+                    console.log('err 2',err)
+                }
+            }))
         }
     }catch(err){
         if(err.code!='ENOENT'){
